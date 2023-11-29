65594e5ea628fb746b89811ebcea15f20979e66b
Julio Cesar Tadeu dos Santos da Silva <julio.c.tadeu@gmail.com>
2023-11-29 12:22:52 -0300

    Questão 1

---
+++ ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria {
+        private int Numero {get;}
+        public string Titular {get; set;}
+        public double Saldo {get; set;}
+       
+        public ContaBancaria(int _numero, string _titular)
+        {
+            Numero = _numero;
+            Titular = _titular;   
+        }
+
+        public ContaBancaria(int _numero, string _titular, 
+            double _depositoInicial) : this(_numero, _titular)
+        {
+            Saldo = _depositoInicial;
+        }
+
+        public void Saque(double valor)
+        {
+            double taxa = 3.5;
+            if(valor > 0)
+                this.Saldo -= (valor+taxa);
+        }
+        public void Deposito(double valor)
+        {
+            if(valor > 0)
+                this.Saldo += valor;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Conta {0}, Titular: {1}, Saldo: $ {2}",
+                        this.Numero, this.Titular, 
+                        this.Saldo.ToString("0.00", CultureInfo.InvariantCulture));
+        }
+    }
+}
